5caf6a417b2d20d5e22422ce7d78c16133edeed1
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-07-14 16:13:43 +0300

    Unit Tests for clean code

---
+++ FizzBuzz/FizzBuzz.Game/FizzBozzBoard.cs
@@ -0,0 +1,23 @@
+﻿namespace FizzBuzz.Game
+{
+    public class FizzBuzzBoard
+    {
+        public string GetWordForNumber(int number)
+        {
+            if (number % 15 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            //return number != 3 ? "Buzz" : "Fizz";
+            return string.Empty;
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzzTests/GameTest.cs
@@ -0,0 +1,70 @@
+﻿using FizzBuzz.Game;
+
+namespace FizzBuzzTests
+{
+    public class GameTest
+    {
+        //[Fact]
+        //public void IsExist()
+        //{
+        //    var gameBoard = new FizzBuzz.Game.FizzBuzzBoard();
+        //    var number = 3;
+        //    string response = gameBoard.GetWordForNumber(number);
+        //}
+
+        FizzBuzzBoard fizzBuzzBoard = new FizzBuzzBoard();
+
+        /*
+         * Ben bir oyuncu olarak
+         * 3 sayısını verdiğimde
+         * Fizz değerini almalıyım.
+         */
+
+        [Fact]
+        public void When_Send_Three_Return_Fizz()
+        {
+            var number = 3;
+            string actual = fizzBuzzBoard.GetWordForNumber(number);
+            Assert.Equal("Fizz", actual);
+
+        }
+        [Fact]
+        public void When_Send_Five_Return_Buzz()
+        {
+            var number = 5;
+            string actual = fizzBuzzBoard.GetWordForNumber(number);
+            Assert.Equal("Buzz", actual);
+
+        }
+        [Theory]
+        [InlineData(6)]
+        [InlineData(9)]
+        [InlineData(12)]
+        public void When_Send_Multiply_three_Return_Fizz(int number)
+        {
+            string actual = fizzBuzzBoard.GetWordForNumber(number);
+            Assert.Equal("Fizz", actual);
+        }
+
+        [Theory]
+        [InlineData(10)]
+        [InlineData(20)]
+        [InlineData(25)]
+        public void When_Send_Multiply_five_Return_Fizz(int number)
+        {
+            string actual = fizzBuzzBoard.GetWordForNumber(number);
+            Assert.Equal("Buzz", actual);
+        }
+
+        [Theory]
+        [InlineData(15)]
+        [InlineData(30)]
+        [InlineData(45)]
+        public void When_Send_Multiply_fifteen_Return_Fizz(int number)
+        {
+            string actual = fizzBuzzBoard.GetWordForNumber(number);
+            Assert.Equal("FizzBuzz", actual);
+        }
+
+    }
+}
